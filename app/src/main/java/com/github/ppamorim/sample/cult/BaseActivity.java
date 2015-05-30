4200993c111b80a70acc710812ae368bb77e27b7
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2015-05-30 15:48:07 -0300

    Structural changes

--- app/src/main/java/com/github/ppamorim/sample/cult/BaseActivity.java
+++ app/src/main/java/com/github/ppamorim/sample/cult/BaseActivity.java
@@ -1,13 +1,37 @@
 package com.github.ppamorim.sample.cult;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.github.ppamorim.cult.CultView;
 
-public class BaseActivity extends ActionBarActivity {
+public class BaseActivity extends AppCompatActivity {
+
+  @InjectView(R.id.cult_view) CultView cultView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_base);
+    ButterKnife.inject(this);
   }
 
+  @Override protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    new Handler().postDelayed(new Runnable() {
+      @Override public void run() {
+        cultView.showSecondView();
+      }
+    }, 1000);
+  }
+
+  @Override public void onBackPressed() {
+    if(cultView.isSecondViewAdded()) {
+      cultView.hideSecondView();
+      return;
+    }
+    super.onBackPressed();
+  }
 }
--- cult/src/main/java/com/github/ppamorim/cult/CultView.java
+++ cult/src/main/java/com/github/ppamorim/cult/CultView.java
@@ -2,20 +2,28 @@ package com.github.ppamorim.cult;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import com.dd.ShadowLayout;
+import com.github.ppamorim.cult.util.SlideHelper;
 import com.github.ppamorim.cult.util.ViewUtil;
 
 public class CultView extends FrameLayout {
 
-  private int imagePadding;
+  private boolean isAnimationRunning;
+
+  private int toolbarHeight;
 
   private ShadowLayout shadowLayout;
-  private RelativeLayout innerView;
-  private ImageView searchIcon;
+  private FrameLayout innerView;
+  private Toolbar innerToolbar;
+  private Toolbar outToolbar;
+  private FrameLayout content;
+
+  private SlideHelper slideHelper;
 
   public CultView(Context context) {
     this(context, null);
@@ -28,49 +36,69 @@ public class CultView extends FrameLayout {
   public CultView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
     config();
-    if(attrs != null && defStyleAttr != 0) {
+    if(attrs != null) {
       getStyle(context, attrs, defStyleAttr);
     }
   }
 
   @Override protected void onFinishInflate() {
     super.onFinishInflate();
-    if(imagePadding != 0) {
-      configSearchButton();
-    }
+    configSizes();
+    configSlideHelper();
   }
 
   private CultView config() {
     addView(inflate(getContext(), R.layout.layout_cult, null));
     shadowLayout = (ShadowLayout) findViewById(R.id.shadow_layout);
-    innerView = (RelativeLayout) findViewById(R.id.inner_view);
-    searchIcon = (ImageView) findViewById(R.id.search_icon);
+    innerView = (FrameLayout) findViewById(R.id.inner_view);
+    innerToolbar = (Toolbar) findViewById(R.id.inner_toolbar);
+    outToolbar = (Toolbar) findViewById(R.id.out_toolbar);
+    content = (FrameLayout) findViewById(R.id.content);
     return this;
   }
 
-  public CultView setBorderRadius(int radius) {
-    //shadowLayout.set
-    return this;
+  public void configSlideHelper() {
+    if(slideHelper == null) {
+      slideHelper = new SlideHelper(getContext(), this);
+    }
   }
 
-  public CultView setInnerPadding(int left, int top, int right, int bottom) {
-    shadowLayout.setPadding(
-        ViewUtil.dpToPx(getResources(), left),
-        ViewUtil.dpToPx(getResources(), top),
-        ViewUtil.dpToPx(getResources(), right),
-        ViewUtil.dpToPx(getResources(), bottom));
-    return this;
-  }
-
-  public CultView configSearchButton() {
-    searchIcon.setPadding(imagePadding, imagePadding, imagePadding, imagePadding);
-    return this;
+  private void configSizes() {
+    RelativeLayout.LayoutParams layoutParams =
+        new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, toolbarHeight);
+    shadowLayout.setLayoutParams(layoutParams);
+    outToolbar.setLayoutParams(layoutParams);
   }
 
   private void getStyle(Context context, AttributeSet attrs, int defStyleAttr) {
     TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.cult_view, defStyleAttr, 0);
-    imagePadding = a.getInt(R.styleable.cult_view_search_padding, 100);
+    toolbarHeight = (int) a.getDimension(R.styleable.cult_view_toolbar_height, 100);
     a.recycle();
+  }
+
+  public CultView setInnerPadding(int left, int top, int right, int bottom) {
+    shadowLayout.setPadding(ViewUtil.dpToPx(getResources(), left),
+        ViewUtil.dpToPx(getResources(), top), ViewUtil.dpToPx(getResources(), right),
+        ViewUtil.dpToPx(getResources(), bottom));
+    return this;
+  }
+
+  public void showSecondView() {
+    slideHelper.slideInTop(outToolbar);
+    slideHelper.slideInBottom(content);
+  }
+
+  public void hideSecondView() {
+    slideHelper.slideOutTop(outToolbar);
+    slideHelper.slideOutBottom(content);
+  }
+
+  public boolean isSecondViewAdded() {
+    return outToolbar.getVisibility() == VISIBLE && content.getVisibility() == VISIBLE;
+  }
+
+  public void resetAnimation() {
+    isAnimationRunning = false;
   }
 
 }
